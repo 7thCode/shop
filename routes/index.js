d41d549d2cb9e3ee2f289833358edc81e1d9c01b
oda <oda.mikio@gmail.com>
2018-08-06 16:31:28 +0900

    mailer
    mailer

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var DocSchema = new Schema({
@@ -18,10 +17,6 @@ var Doc = mongoose.model('Doc', DocSchema);
 
 mongoose.connect("mongodb://localhost/test", {});
 
-// index
-
-// localhost:3000/insert/namae/atai
-
 router.post('/api/contact', function (request, response, next) {
 
     var email = request.body.email;
@@ -32,89 +27,11 @@ router.post('/api/contact', function (request, response, next) {
 
     var postdata = new Doc({email: email, fname: fname, lname: lname, subject: subject, message: message});
 
-//    var postdata = new Doc(request.body);
-
     postdata.save().then(function (saved_doc) {
         response.json(saved_doc);
     });
 
 });
-
-/*
-var send_mail = function () {
-
-    this.mailer = require('nodemailer');
-
-    var mailsetting = {
-        "type": "gmail",
-        "account": "inbox.7thcode@gmail.com",
-        "setting": {
-            "service": "gmail",
-            "auth": {
-                "user": "inbox.7thcode@gmail.com",
-                "pass": "33550336"
-            }
-        }
-    };
-
-    var smtp_user = mailer.createTransport('SMTP', mailsetting); //SMTPの接続
-
-    var result_mail = {
-        from: account,
-        to: mail_address,
-        bcc: bcc_address,
-        subject: title,
-        html: message
-    };
-
-    smtp_user.sendMail(result_mail, function (error) {
-        callback(error);
-        smtpUser.close();
-    });
-
-};
-*/
-
-/*
-router.get('/api/contact/:email', function (request, response, next) {
-
-    var email = decodeURIComponent(request.params.email);
-
-    Doc.findOne({email: email}).then(function (doc) {
-        response.json(doc);
-    });
-
-});
-
-router.get('/api/query', function (request, response, next) {
-
-    Doc.find({}).then(function (docs) {
-        response.json(docs);
-    });
-
-});
-
-router.put('/api/contact/:email', function (request, response, next) {
-
-    var email = decodeURIComponent(request.params.email);
-    var value = request.params.value;
-
-    Doc.findOneAndUpdate({email: email}, {$set:{value: value}}).then(function (updated_doc) {
-        response.json(updated_doc);
-    })
-
-});
-
-router.delete('/api/contact/:email', function (request, response, next) {
-
-    var email = decodeURIComponent(request.params.email);
-
-    Doc.findOneAndRemove({email: email}).then(function () {
-        response.json({});
-    })
-
-});
-*/
 
 module.exports = router;
 
